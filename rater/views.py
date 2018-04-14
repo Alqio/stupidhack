a6fa53569b36ddc560b5017403d72f709640f037
Sofia Kimpimäki <kimpims@aalto.fi>
2018-04-14 11:26:22 +0300

    mandarin rated

--- rater/views.py
+++ rater/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from . import forms
-
+from .models import (Mandarin)
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'RateCount': Mandarin.objects.count()})
 
 def slider(request):
     return render(request, 'slider.html')
@@ -11,9 +11,11 @@ def slider(request):
 def rate(request):
     if request.method == 'POST':
         form = forms.RateMandarin(request.POST)
-        
+
+
         if form.is_valid():
             form.save()
+
 
             return redirect('/eatornot')
     else:
@@ -24,3 +26,5 @@ def rate(request):
 
 def eatornot(request):
     return HttpResponse("EAT IT! or don't")
+
+
