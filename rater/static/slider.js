2c2cd97ed2f2fdd7c2be55f2db708d98771bfa29
Elisa <elisa.naskali@gmail.com>
2018-04-14 11:31:42 +0300

    Better slider

--- rater/static/slider.js
+++ rater/static/slider.js
@@ -6,6 +6,10 @@ function nextQuestion() {
     showQuestion(questionNo += 1);
 }
 
+function prevQuestion() {
+    showQuestion(questionNo -= 1);
+}
+
 function currentQuestion(n) {
     showQuestion(questionNo = n);
 }
@@ -14,8 +18,10 @@ function showQuestion(n) {
     var i;
     var questions = document.getElementsByClassName('questionCard');
 
-    if (n > questions.length || n < 1) {
+    if (n > questions.length) {
         document.getElementById("rateButton").style.visibility = "visible";
+    } else if (n < 1){
+        questionNo = 1;
     }
 
     for (i = 0; i < questions.length; i++) {
